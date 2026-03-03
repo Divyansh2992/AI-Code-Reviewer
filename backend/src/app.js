145ac2c0358e6fe53e7f9b0fad3698eb8ff97685
Divyansh2992 <Divyanshvijay92@gmail.com>
2026-03-03 18:26:21 +0530

    fixed  cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin: ["https://ai-code-reviewer-ebon.vercel.app", "http://localhost:5173", "http://localhost:5174"]
+    origin: ["https://ai-code-reviewer-ebon.vercel.app", "https://ai-code-reviewer-git-main-divyansh2992s-projects.vercel.app", "http://localhost:5173", "http://localhost:5174"]
 }));
 
 app.use("/ai", (aiRoutes));
